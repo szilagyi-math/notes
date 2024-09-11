f7231531f790edfbe2f9aaa6c8760c177509c289
Tibor Sándor <tiborsandor2001@gmail.com>
2024-09-11 21:07:29 +0200

    Use the actual last update date instead of \today

--- scripts/shell/compile.ts
+++ scripts/shell/compile.ts
@@ -26,6 +26,37 @@ cp ${subject.buildDir}/${subject.book.source.replace(
 # Change to the practice directory
 cd ../${subject.practiceMaterial.dir}
 
+# Change today to the last updated date
+for file in $(find . -name "*.tex"); do
+  # Get the year, month (number), and day
+  year=$(git log -1 --format="%ad" --date=format:"%Y" -- "$file")
+  month_num=$(git log -1 --format="%ad" --date=format:"%m" -- "$file")
+  day=$(git log -1 --format="%ad" --date=format:"%d" -- "$file")
+  
+  # Map month numbers to Hungarian names
+  case "$month_num" in
+    "01") month="január";;
+    "02") month="február";;
+    "03") month="március";;
+    "04") month="április";;
+    "05") month="május";;
+    "06") month="június";;
+    "07") month="július";;
+    "08") month="augusztus";;
+    "09") month="szeptember";;
+    "10") month="október";;
+    "11") month="november";;
+    "12") month="december";;
+  esac
+  
+  # Format the final Hungarian date
+  hungarian_date="$year. $month $day."
+
+  # Use sed with # as a delimiter
+  sed -i '' "s/\\\\\\today/$hungarian_date/g" $file
+done
+
+
 # Run latexmk with the config file and copy the files to the ${docsDir} directory
 ${subject.practiceMaterial.files
   .map(file => {
