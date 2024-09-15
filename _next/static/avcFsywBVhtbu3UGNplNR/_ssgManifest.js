e5c6511921113eaa4ae9c9e8284bd84edbb48ae1
tibi1220 <42047078+tibi1220@users.noreply.github.com>
2024-09-15 19:33:22 +0000

    Deploying to gh-pages from @ szilagyi-math/notes@ecae9fb978cdd1778f6c6825bec80823f6fd529f 🚀

--- _next/static/avcFsywBVhtbu3UGNplNR/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fsubjects\u002F[subjectCode]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/to0Nlu8DSGSCoqNDfFQ36/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fsubjects\u002F[subjectCode]","\u002Fsubjects\u002F[subjectCode]\u002Fnotes\u002F[...noteSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
