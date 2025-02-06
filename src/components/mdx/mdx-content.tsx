d9e1a013a51fa8b84f5f4fc9fd7cb6da44dceada
Tibor Sandor <tiborsandor2001@gmail.com>
2025-02-06 13:17:52 +0100

    ADd overscroll to mdx components

--- src/components/mdx/mdx-content.tsx
+++ src/components/mdx/mdx-content.tsx
@@ -38,7 +38,7 @@ const MDXContent = ({ code }: MDXContentProps) => {
           const [, chapter, section] = props.id.split('.');
 
           return (
-            <div className='border-l-4 border-red-600 pl-3 pr-2 py-1 bg-secondary-bg/35 my-5 *:last:mb-0 *:first:mt-0'>
+            <div className='border-l-4 border-red-600 pl-3 pr-2 py-1 bg-secondary-bg/35 my-5 *:last:mb-0 *:first:mt-0 overflow-x-auto'>
               <h4 id={props.id}>
                 Definíció {`${chapter}.${section}`}
                 {props.title ? ` [ ${props.title} ]` : null}
@@ -61,31 +61,31 @@ const MDXContent = ({ code }: MDXContentProps) => {
           );
         },
         Proof: (props: any) => (
-          <div className='mt-4 border-t pt-2 *:last:mb-0 *:first:mt-0'>
+          <div className='mt-4 border-t pt-2 *:last:mb-0 *:first:mt-0 overflow-x-auto'>
             <h4>Bizonyítás {props.title ? `[ ${props.title} ]` : null} </h4>
             {props.children}
           </div>
         ),
         BlueBox: (props: any) => (
-          <div className='pl-3 pr-2 border-l-4 border-blue-600 py-2 my-5 bg-secondary-bg/35 *:last:mb-0 *:first:mt-0'>
+          <div className='pl-3 pr-2 border-l-4 border-blue-600 py-2 my-5 bg-secondary-bg/35 *:last:mb-0 *:first:mt-0 overflow-x-auto'>
             {props.children}
           </div>
         ),
         Example: (props: any) => (
           // TODO: Create example css class
-          <div className='pl-3 pr-2 border-l-4 border-sky-600 py-2 my-5 bg-secondary-bg/35 *:last:mb-0 *:first:mt-0'>
+          <div className='pl-3 pr-2 border-l-4 border-sky-600 py-2 my-5 bg-secondary-bg/35 *:last:mb-0 *:first:mt-0 overflow-x-auto'>
             <h4>Példa</h4>
             {props.children}
           </div>
         ),
         Note: (props: any) => (
-          <div className='pl-3 pr-2 border-l-4 border-amber-400 py-2 my-5 bg-secondary-bg/35 *:last:mb-0 *:first:mt-0'>
+          <div className='pl-3 pr-2 border-l-4 border-amber-400 py-2 my-5 bg-secondary-bg/35 *:last:mb-0 *:first:mt-0 overflow-x-auto'>
             <h4>Megjegyzés</h4>
             {props.children}
           </div>
         ),
         Statement: (props: any) => (
-          <div className='pl-3 pr-2 border-l-4 border-purple-600 bg-secondary-bg/35 py-2 my-5 *:last:mb-0 *:first:mt-0'>
+          <div className='pl-3 pr-2 border-l-4 border-purple-600 bg-secondary-bg/35 py-2 my-5 *:last:mb-0 *:first:mt-0 overflow-x-auto'>
             <h4>Állítás</h4>
             {props.children}
           </div>
